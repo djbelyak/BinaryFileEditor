80d65def70c1856a4a1e1dcaaf72630bd8ff7ba7
Иван Белявцев <djbelyak@gmail.com>
2013-10-03 21:13:49 +0400

    Hello world project

---
+++ Debug/src/subdir.mk
@@ -0,0 +1,24 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CPP_SRCS += \
+../src/BinaryFileEditor.cpp 
+
+OBJS += \
+./src/BinaryFileEditor.o 
+
+CPP_DEPS += \
+./src/BinaryFileEditor.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+src/%.o: ../src/%.cpp
+	@echo 'Building file: $<'
+	@echo 'Invoking: Cross G++ Compiler'
+	g++ -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
---
+++ src/BinaryFileEditor.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : BinaryFileEditor.cpp
+// Author      : Elka-vt
+// Version     :
+// Copyright   : 
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	return 0;
+}
